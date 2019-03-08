c7f4bc8b83fe7b798e3457092863791e6beab64e
Dylan Rankin <dylansr90@gmail.com>
2019-03-08 23:11:46 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Repository for HCAL reconstruction using HLS4ML
+
+Adapted for AXI-stream inputs from https://github.com/drankincms/hls4ml_c
---
+++ hls4ml_hcal.h
@@ -0,0 +1,11 @@
+#ifndef HLS4ML_HCAL_H_
+#define HLS4ML_HCAL_H_
+
+#include "parameters.h"
+
+void hls4ml_hcal(
+        const data32_t *in, // Read-Only Vector
+        data32_t *out       // Output Result
+        );
+
+#endif
